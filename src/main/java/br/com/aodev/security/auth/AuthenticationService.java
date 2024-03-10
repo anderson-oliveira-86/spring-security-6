8d455ffc466cdd0710828da5c7e4c866063f7869
Anderson de Oliveiram <anderson.java.dev@gmail.com>
2024-03-10 20:00:03 -0300

    implementando configuracao de CORS

--- src/main/java/br/com/aodev/security/auth/AuthenticationService.java
+++ src/main/java/br/com/aodev/security/auth/AuthenticationService.java
@@ -20,8 +20,8 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
-        var user = User.builder().firstname(request.getFirstName())
-                .lastname(request.getLastName())
+        var user = User.builder().firstname(request.getFirstname())
+                .lastname(request.getLastname())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .role(Role.USER)
